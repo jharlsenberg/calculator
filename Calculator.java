d500e48ac44aabcd876ca1e821b186e113fc737b
jharlsenberg <josh@harlenokeeffe.com>
2022-04-14 13:55:33 +1000

    Updated calculator funciton

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,9 @@
-public class Calculator {}
-public class Calculator() {
- public static void main(String[] args) {}
- }
 public class Calculator {
  public int addition(int a, int b) {
  return a + b;
  }
+
+ public double addition(double a, double b) {
+ return a + b;
  }
+}
